989bb4c8b8c5e2f95208451f105722310cc6b963
sorrrb <cdrukepike@gmail.com>
2024-01-11 14:49:41 -0500

    Refactor repo skeleton for OOP emphasis

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
 import './style.css';
-import createTask from './modules/projects';
-import loadPage from './pageload';
-
-function init() {
-  // Create initial content container
-  const container = document.createElement('div');
-  container.id = 'content';
-  document.body.appendChild(container);
-
-  const test = createTask('Title', 'Description', '01-10-2024', 0, 'N/A', 'Default');
-  console.log(test.getTitle());
-  console.log(test.getDueDate());
-  
-  loadPage();
-}
-
-init(); // Initial pageload
