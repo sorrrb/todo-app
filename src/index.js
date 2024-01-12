ddc30f532598dde6ae60c43aa26a7ae0614549d0
sorrrb <cdrukepike@gmail.com>
2024-01-12 16:40:10 -0500

    Add font assets & refactor logic

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
   - title [str]
   - description [str]
   - dueDate [str]
-  - priority [num]
+  - priority [num] [0, 1, 2, 3] [no deadline, low, medium, high priority]
   - notes [str]
   - checklist [arr]
 
@@ -16,38 +16,7 @@
 
 */
 
+import { createList, createTask } from './modules/task';
 import './style.css';
-import { createTask, createList } from './modules/task';
 
 /* Main file where code execution occurs */
-
-function init() {
-  // [START]
-  const defaultList = createList('Default'); // Create default project list
-  console.log(`-- Created Default List --`);
-  const sampleTask = createTask('Complete Project', 'Work on Todo application', 'Tomorrow', 'High', 'N/A', 'empty'); // Create sample task to test list mutation
-  console.log(`-- Created Sample Task --`);
-
-  // Dummy code for creating button to show default list tasks
-  const logList = document.createElement('button');
-  logList.textContent = 'Log Default List to console';
-  document.body.appendChild(logList);
-  // Dummy code for handling button event
-  logList.addEventListener('click', e => {
-    console.log(defaultList.getTasks())
-  });
-
-  // Dummy code for creating button to add task to default list
-  const logAdd = document.createElement('button');
-  logAdd.textContent = 'Add Test List to Default List';
-  document.body.appendChild(logAdd);
-
-  logAdd.addEventListener('click', e => {
-    console.log('Adding task to list');
-    defaultList.addTask(sampleTask);
-    console.log(defaultList.getTasks());
-  });
-  // [END]
-}
-
-init();
--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,16 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-      }
-    ]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
   optimization: {
     runtimeChunk: 'single',
