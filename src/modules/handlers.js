b1596a5a4157a94d4c46b67f05aa6f4cc6ee9843
sorrrb <cdrukepike@gmail.com>
2024-01-30 20:02:47 -0500

    Implement basic tab switching logic for projects

--- src/modules/handlers.js
+++ src/modules/handlers.js
@@ -1,6 +1,9 @@
 import CloseIcon from '../assets/media/close.svg';
+import FolderIcon from '../assets/media/folder.svg';
+import { projectManager, createProject, createTodo } from '../modules/task';
+import { createProjectFolder, createProjectHeader } from '../modules/load';
 
-const displayManager = (function () {
+const displayManager = (function () { // Use this to display todos???
   return {
 
   }
@@ -12,6 +15,22 @@ function toggleModalDisplay() { // Toggles modal display value
   modal.classList.toggle('active');
 }
 
+function generateProjectFolders() {
+  const content = document.getElementById('user-projects');
+  content.innerHTML = '';
+
+  const folders = projectManager.getProjects();
+
+  const header = createProjectHeader(folders.length);
+  content.appendChild(header);
+
+  folders.forEach((folder) => {
+    const tab = createProjectFolder(folder.getTitle(), FolderIcon, folder.getId());
+    tab.addEventListener('click', tabHandler);
+    content.appendChild(tab);
+  })
+}
+
 function submitProjectModal() {
   const content = document.getElementById('modal-content');
   const input = content.querySelector('input').value;
@@ -21,6 +40,16 @@ function submitProjectModal() {
     warning.style.display = 'block';
     return;
   }
+
+  const projects = projectManager.getProjects();
+
+  const newProject = createProject(input, projects.length);
+  projectManager.addProject(newProject);
+  generateProjectFolders();
+  toggleModalDisplay();
+
+  const createProjectBtn = document.getElementById('create-project'); // Reimplement event listener for create project button
+  createProjectBtn.addEventListener('click', createHandler);
 }
 
 function generateProjectModal() {
@@ -87,10 +116,24 @@ function createHandler() { // Handles logic after pressing create project button
   if (this.id === 'create-project') generateProjectModal();
 }
 
+function tabHandler() {
+  const projects = document.querySelectorAll('div.project-folder');
+  projects.forEach((project) => {
+    if (project.classList.contains('active-folder')) project.classList.toggle('active-folder');
+  });
+  this.classList.toggle('active-folder');
+  console.log(this.hasAttribute('data-index')); // log boolean value if folder clicked was user project folder
+}
+
 export function manageEventListeners() {
   const defaultActiveProject = document.querySelector('div.project-folder'); // Sets default active project to All
   defaultActiveProject.classList.add('active-folder');
 
   const createProjectBtn = document.getElementById('create-project');
   createProjectBtn.addEventListener('click', createHandler);
+
+  const projects = document.querySelectorAll('div.project-folder');
+  projects.forEach((project) => {
+    project.addEventListener('click', tabHandler);
+  });
 }
